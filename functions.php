79fd27afd455e758976c1e4ef0986a4a2049afb5
Chaves Pereira <chavespereira@gmx.fr>
2017-05-22 21:49:32 +0200

    Ajouté support couleur du fond au thème

--- functions.php
+++ functions.php
@@ -54,3 +54,15 @@ function add_widget()
     ));
 
 }
+
+$defaults = array(
+    'default-color'          => '',
+    'default-image'          => '',
+    'default-repeat'         => '',
+    'default-position-x'     => '',
+    'default-attachment'     => '',
+    'wp-head-callback'       => '_custom_background_cb',
+    'admin-head-callback'    => '',
+    'admin-preview-callback' => ''
+);
+add_theme_support( 'custom-background', $defaults );
