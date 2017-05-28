33c34411e58b62db9d4bc4566d16f118416cc960
Chaves Pereira <chavespereira@gmx.fr>
2017-05-28 16:48:26 +0200

    Ajouté images en avant dans la page Accueil

--- home.php
+++ home.php
@@ -1,25 +1,31 @@
 <?php get_header(); ?>
 
 <div class="container-fluid">
-<?php
-
-	$the_query = new WP_Query(array('category_name' => 'Accueil'));
-	setup_postdata( $post );
-	while ($the_query->have_posts() ) {
-		$the_query->the_post();
-		
-		?>
-		<div class="accueil-paragraphe">
-			<h2><?php the_title(); ?></h2>
-			<?php the_content(); ?>
-
-		</div>
-			
 	<?php
-	
-	}
-?>
-</div>
+		$the_query = new WP_Query(array('category_name' => 'Accueil'));
+		setup_postdata( $post );
+		while ($the_query->have_posts() ) {
+			$the_query->the_post();
+			
+			?>
+			<div class="row">
+				<h2><?php the_title(); ?></h2>
+				<div class="col-md-8">
+					<p><?php the_content(); ?></p>
+				</div>
+				
+				<?php
+				if (has_post_thumbnail())
+					{ ?>
+						<div class="col-md-2">
+						<?php the_post_thumbnail(); ?>
+						</div> <?php
+					} ?>
+			</div>
+			<?php	
+		}
+	?>
+	</div>
 <div class="container-fluid">
 	<div class="row">
 		<div class="banniere">
