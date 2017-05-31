29f9a290b95f5981f962d607510d2abcdc46802c
Chaves Pereira <chavespereira@gmx.fr>
2017-05-31 14:50:29 +0200

    Modifié footer

--- footer.php
+++ footer.php
@@ -1,11 +1,16 @@
 ﻿
 	<footer class="container-fluid">
-
+	<div class="row">
+		<div class="col-md-offset-3 col-md-6 col-xs-12">
+			<?php
+			dynamic_sidebar('widget_footer');
+			?>
+		</div>
+	</div>
 		<div class="row">
-			<div id="social" class="col-md-2 col-xs-12">
+			<div class="col-md-2 col-xs-12 social">
 				<a href="#" class="btn btn-default" ><i class="fa fa-facebook fa-2x"></i></a>
 				<a href="#" class="btn btn-default" ><i class="fa fa-twitter fa-2x"></i></a>
-				<a href="#" class="btn btn-default"><i class="fa fa-youtube fa-2x"></i></a>
 			</div>
 
 			<nav  class="navbar navbar-default col-md-offset-2 col-md-4 col-xs-12">
@@ -13,11 +18,7 @@
 										'menu_class' => 'nav navbar-nav'
 				));  ?>
 			</nav>
-			<div class="col-md-offset-2 col-md-2 col-xs-12">
-				<?php
-				dynamic_sidebar('widget_footer');
-				?>
-			</div>
+
 		</div>
 		<?php wp_footer(); ?>
 	</footer>
