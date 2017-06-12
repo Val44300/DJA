5be6abdb8fb6ff03465a8cb43bda7824f80496d5
Chaves Pereira <chavespereira@gmx.fr>
2017-06-12 19:13:58 +0200

    Ajouté 404.php

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); 
+	?>
+<div class="error-404" style="text-align: center;">
+<p class="error-404-text">Erreur 404</p>
+	<img src="http://localhost/wp-test/wp-content/themes/DJA/images/404.jpg">
+	<p>Cette page a probablement été atomisée pour avoir dit du mal des développeurs du site</p>
+</div>
+
+<?php
+ get_footer();
